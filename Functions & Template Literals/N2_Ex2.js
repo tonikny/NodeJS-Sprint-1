e12fe94b41f6650e50571dabe0edaab973165d16
tonikny <dalecanya@gmail.com>
2021-11-02 12:29:15 +0100

    Correcció N2_Ex2.js

--- Functions & Template Literals/N2_Ex2.js
+++ Functions & Template Literals/N2_Ex2.js
@@ -1,8 +1,9 @@
 
-function escriu_nom (nom, cognom) {
-  return `Hola, soc ${nom} ${cognom}`;
+function escriu_nom (n, c) {
+  return `${n} ${c}`;
 }
 
 const nom = "Toni";
 const cognom = "Muelas";
-console.log(escriu_nom(nom, cognom));
+
+console.log(`Hola, soc ${escriu_nom(nom, cognom)}`);
