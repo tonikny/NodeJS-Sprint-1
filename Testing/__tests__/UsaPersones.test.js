d06f024f0d0da5636e78dbf94a0b4b20fc7e1a1e
tonikny <dalecanya@gmail.com>
2021-11-10 11:24:16 +0100

    Update UsaPersones.test.js

--- Testing/__tests__/UsaPersones.test.js
+++ Testing/__tests__/UsaPersones.test.js
@@ -1,6 +1,8 @@
 const UsaPersones = require('../app/UsaPersones');
 const Persona = require('../app/Persona');
 
+
+// Preparem el mock de la classe Persona
 jest.mock('../app/Persona');
 
 const mockDirNom = jest.fn();
@@ -10,28 +12,41 @@ Persona.mockImplementation(() => {
   };
 });
 
-beforeEach(() => {
-  Persona.mockClear();
-  mockDirNom.mockClear();
-});
 
-test('UsaPersones pot fer new() sense errors', () => {
-  const usaPersones = new UsaPersones();
-  expect(usaPersones).toBeTruthy(); // Constructor ran with no errors
-});
+describe ('Tests Persona', () => {
 
-test('Si no hi ha parametres no es crida a Persona i si nhi ha, si', () => {
-  let usaPersones = new UsaPersones();
-  expect(Persona).not.toHaveBeenCalled();
-  usaPersones = new UsaPersones(["Toni", "Pep"]);
-  expect(Persona).toHaveBeenCalled();
-});
-
-test('Es crida a dirNom() un cop per cada item de la llista', () => {
-  const llista = ["Toni", "Pep", "Pere"];
-  const usaPersones = new UsaPersones(llista);
-  usaPersones.llistaPersones();
-  expect(mockDirNom).toHaveBeenCalledTimes(3);
+  test('dirNom', () => {
+    const Persona = jest.requireActual('../app/Persona'); // fem servir la classe no mockeada
+    const p = new Persona('Toni');
+    expect(p.dirNom()).toEqual('Toni');
+  });
 });
 
 
+describe ('Mock Tests', () => {
+
+  beforeEach(() => {
+    Persona.mockClear();
+    mockDirNom.mockClear();
+  });
+
+  test('UsaPersones pot fer new() sense errors', () => {
+    const usaPersones = new UsaPersones();
+    expect(usaPersones).toBeTruthy(); // Constructor ran with no errors
+  });
+
+  test('Si no hi ha parametres no es crida a Persona i si nhi ha, si', () => {
+    let usaPersones = new UsaPersones();
+    expect(Persona).not.toHaveBeenCalled();
+    usaPersones = new UsaPersones(["Toni", "Pep"]);
+    expect(Persona).toHaveBeenCalled();
+  });
+
+  test('Es crida a dirNom() un cop per cada item de la llista', () => {
+    const llista = ["Toni", "Pep", "Pere"];
+    const usaPersones = new UsaPersones(llista);
+    usaPersones.llistaPersones();
+    expect(mockDirNom).toHaveBeenCalledTimes(3);
+  });
+
+});
