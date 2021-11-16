25dc4b0a78331ae5d53ff9d143d84a8655b976d1
tonikny <dalecanya@gmail.com>
2021-11-16 13:01:53 +0100

    Correccio Classes N3_Ex1.js

--- Classes & Arrow Functions/N3_Ex1.js
+++ Classes & Arrow Functions/N3_Ex1.js
@@ -10,19 +10,31 @@ class Abs {
 
 // Classe concreta
 class Miclasse extends Abs {
+  saluda() {
+    console.log('Hola');
+  }
 }
 
-
-function creaObjectes () {
+function creaObjecteAbs () {
   try {
     // instanciar classe abstracta -> error
     const c = new Abs();
   } catch (err) {
     console.error(err.message);
   }
-  // instanciar classe concreta
-  const mc = new Miclasse();
-  console.log('Miclasse instanciada');
 }
 
-creaObjectes();
+function creaObjecteMiclasse () {
+  try {
+    // instanciar classe concreta
+    const mc = new Miclasse();
+    console.log('Miclasse instanciada');
+    return mc;
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+const abs = creaObjecteAbs();
+const mic = creaObjecteMiclasse();
+mic.saluda();
